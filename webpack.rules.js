b086471fdb47db393f40dddb690d8c8bc8a2d9b8
mahirminhajk <kmahirminhaj@gmail.com>
2024-01-05 01:34:21 +0530

    add react package and edited webpack.rules.js

--- webpack.rules.js
+++ webpack.rules.js
@@ -16,6 +16,17 @@ module.exports = [
       },
     },
   },
+  //* react
+  {
+    test: /\.jsx?$/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        exclude: /node_modules/,
+        presets: ['@babel/preset-react']
+      }
+    }
+  },
   // Put your webpack loader rules in this array.  This is where you would put
   // your ts-loader configuration for instance:
   /**
